6e7a54c77ee1a5dbe96289664ed636f48931ab10
lyptt <account+github@lyptt.uk>
2016-12-24 16:07:33 +0000

    Added remaining tests for Specfile

--- spec/unit/spec_file_spec.rb
+++ spec/unit/spec_file_spec.rb
@@ -16,7 +16,6 @@ RSpec.describe Xcodegen::Specfile do
 			expect(spec.configurations).to include(config)
 		end
 
-
 		it 'creates a parser object when parsing if one has not been provided' do
 			fake_parser_result = {}
 			parse_arg = '1/2/3.yaml'
@@ -31,6 +30,36 @@ RSpec.describe Xcodegen::Specfile do
 			parser = double('parser', parse: fake_parser_result, register: nil, register_defaults: nil)
 
 			expect(Xcodegen::Specfile.parse(parse_arg, parser)).to equal(fake_parser_result)
+		end
+
+		it 'creates a writer object when writing if one has not been provided' do
+			fake_writer_result = {}
+			write_arg = '1/2/3.yaml'
+
+			version = Semantic::Version.new('1.0.0')
+			base_dir = Dir.tmpdir
+			target = Xcodegen::Specfile::Target.new '', '', '', [], [], [], '', []
+			config = Xcodegen::Specfile::Configuration.new '', [], {}, 'debug'
+
+			spec = Xcodegen::Specfile.new version, [target], [config], base_dir
+
+			allow_any_instance_of(Xcodegen::Specwriter).to receive(:write_spec).and_return(fake_writer_result)
+			expect(spec.write(write_arg, nil)).to equal(fake_writer_result)
+		end
+
+		it 'uses an existing writer object when writing if one has been provided' do
+			fake_writer_result = {}
+			write_arg = '1/2/3.yaml'
+
+			version = Semantic::Version.new('1.0.0')
+			base_dir = Dir.tmpdir
+			target = Xcodegen::Specfile::Target.new '', '', '', [], [], [], '', []
+			config = Xcodegen::Specfile::Configuration.new '', [], {}, 'debug'
+
+			spec = Xcodegen::Specfile.new version, [target], [config], base_dir
+			writer = double('writer', write_spec: fake_writer_result, register: nil, register_defaults: nil, write_configuration: nil, write_target: nil)
+
+			expect(spec.write(write_arg, writer)).to equal(fake_writer_result)
 		end
 	end
 end
