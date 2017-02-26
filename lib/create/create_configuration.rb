dbe91251f49c936c1ae63962dfe9856e03deae21
lyptt <account+github@lyptt.uk>
2017-02-26 09:36:41 +0000

    Further rubocop adjustments, refactored everything in create

--- lib/create/create_configuration.rb
+++ lib/create/create_configuration.rb
@@ -13,48 +13,50 @@ module StructCore
 			}.freeze
 
 			def self.run_interactive
+				project_file = query_project_file
+				type = query_configuration_types
+				profiles = query_profiles
+
+				configuration = StructCore::Specfile::Configuration.new(name, profiles, nil, type)
+				run project_file, configuration
+			end
+
+			def self.query_project_file
 				directory = Dir.pwd
-				if File.exist? File.join(directory, 'project.yml')
-					project_file = File.join(directory, 'project.yml')
-				elsif File.exist? File.join(directory, 'project.json')
-					project_file = File.join(directory, 'project.json')
-				else
-					project_file = Ask.input 'Enter the project spec path'
-					unless Pathname.new(project_file).absolute?
-						project_file = File.join(directory, project_file)
-					end
-				end
+				project_file = nil
 
+				project_file = File.join(directory, 'project.yml') if File.exist? File.join(directory, 'project.yml')
+				project_file = File.join(directory, 'project.json') if File.exist? File.join(directory, 'project.json')
+				project_file = Ask.input 'Enter the project spec path' if project_file.nil?
+
+				project_file = File.join(directory, project_file) unless Pathname.new(project_file).absolute?
 				raise StandardError.new 'Unable to locate project file' unless File.exist? project_file
-				spec = StructCore::Specfile.parse(project_file)
-				name = Ask.input 'Enter a configuration name'
-				raise StandardError.new 'Configuration name must be at least 1 character long' unless name != nil && name.length > 0
 
-				configuration_types = ['debug', 'release']
+				project_file
+			end
+
+			def self.query_configuration_types
+				name = Ask.input 'Enter a configuration name'
+				raise StandardError.new 'Configuration name must be at least 1 character long' if name.nil? || name.empty?
+
+				configuration_types = %w(debug release)
 				if configuration_types.include? name
 					type = nil
 				else
-					selected_type = Ask.list 'Please select this configuration\'s type', ['Debug', 'Release']
+					selected_type = Ask.list 'Please select this configuration\'s type', %w(Debug Release)
 					type = configuration_types[selected_type]
 				end
 
-				profiles = Ask.checkbox('Please select one or many configuration profiles', PROFILE_NAMES)
-					.map.with_index { |selection, index|
-						selection ? PROFILE_NAMES[index] : nil
-					}.compact
+				type
+			end
 
-				unless profiles != nil && profiles.length > 0
-					raise StandardError.new 'No platform or configuration profiles were specified'
-				end
+			def self.query_profiles
+				profiles = Ask.checkbox('Please select one or many configuration profiles', PROFILE_NAMES).map.with_index { |selection, index|
+					selection ? PROFILE_NAMES[index] : nil
+				}.compact
 
-				configuration = StructCore::Specfile::Configuration.new(
-					name,
-					profiles,
-					nil,
-					type
-				)
-
-				run project_file, configuration
+				raise StandardError.new 'No platform or configuration profiles were specified' if profiles.nil? || profiles.empty?
+				profiles
 			end
 
 			def self.run(project_file, configuration)
