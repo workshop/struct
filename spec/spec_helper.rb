f9439b9d14ac60b5743716e9fc811ca77516c88e
lyptt <account+github@lyptt.uk>
2017-02-26 15:26:45 +0000

    Attempt to fix CI-only test failures, added CI log

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -10,15 +10,17 @@ end
 RSpec.configure do |config|
 	config.color = true
 	config.tty = true
-	original_stderr = $stderr
-	original_stdout = $stdout
-	config.before(:all) do
-		# Redirect stderr and stdout
-		$stderr = File.open(File::NULL, 'w')
-		$stdout = File.open(File::NULL, 'w')
-	end
-	config.after(:all) do
-		$stderr = original_stderr
-		$stdout = original_stdout
+	if ENV.key? 'CI'
+		original_stderr = $stderr
+		original_stdout = $stdout
+		config.before(:all) do
+			# Redirect stderr and stdout
+			$stderr = File.open(File::NULL, 'w')
+			$stdout = File.open(File::NULL, 'w')
+		end
+		config.after(:all) do
+			$stderr = original_stderr
+			$stdout = original_stdout
+		end
 	end
 end
