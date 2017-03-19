46a2f03d138d132f784776b46da26ada54220daa
lyptt <account+github@lyptt.uk>
2017-03-19 19:08:50 +0000

    Added integration test for product embedding

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -34,6 +34,7 @@ require_relative '../lib/spec/writer/spec_writer_1_0_X'
 require_relative '../lib/spec/writer/spec_writer_1_1_X'
 require_relative '../lib/spec/writer/spec_writer_1_2_X'
 require_relative '../lib/spec/builder/spec_builder'
+require_relative '../lib/xcodeproj/app_embed_generator'
 require_relative '../lib/xcodeproj/xcodeproj_writer'
 
 def copy_support_files(source_dir, dest_dir)
